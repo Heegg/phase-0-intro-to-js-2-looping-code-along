b7e1be97aaa595327ab3dca3855c90652169f80c
Heeji Hyun <wlgmlgus@gmail.com>
2022-01-03 02:01:02 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+const names = [`Guadalupe`, `Ollie`, `Aki`]
+const event = [`surpreise`]
+
+function writeCards(names, event) {
+    for (let i=0; i < names.length; i++) 
+        console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    return (names, event);
+    }
+
+
+writeCards([`Guadalupe`, `Ollie`, `Aki`], `surprise`);
